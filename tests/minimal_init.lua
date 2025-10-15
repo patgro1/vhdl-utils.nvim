fddb176af6203fc296e348cc747b87ffe5179508
Patrick Grogan <po.grogan@gmail.com>
2025-10-14 21:38:55 -0400

    fix ci

--- tests/minimal_init.lua
+++ tests/minimal_init.lua
@@ -1,27 +1,22 @@
--- tests/minimal_init.lua
-local plenary_dir = os.getenv("PLENARY_DIR") or "/tmp/plenary.nvim"
-local treesitter_dir = os.getenv("TREESITTER_DIR") or "/tmp/nvim-treesitter"
+-- Detect if running in CI
+local is_ci = os.getenv("CI") ~= nil
 
--- Clone dependencies if they don't exist
-local function ensure_installed(url, install_path)
-    if vim.fn.isdirectory(install_path) == 0 then
-        print("Installing " .. url .. " to " .. install_path)
-        vim.fn.system({ "git", "clone", "--depth=1", url, install_path })
-    end
+if is_ci then
+    -- CI paths
+    vim.opt.rtp:append("~/.local/share/nvim/site/pack/ci/opt/plenary.nvim")
+    vim.opt.rtp:append("~/.local/share/nvim/site/pack/ci/opt/nvim-treesitter")
+    vim.opt.rtp:append("~/.local/share/nvim/site/pack/ci/opt/vhdl-utils.nvim")
+else
+    -- Local development - use your actual paths
+    local data_path = vim.fn.stdpath("data")
+    vim.opt.rtp:append(data_path .. "/lazy/plenary.nvim")
+    vim.opt.rtp:append(data_path .. "/lazy/nvim-treesitter")
+    vim.opt.rtp:append(".") -- Current directory (plugin root)
 end
 
-ensure_installed("https://github.com/nvim-lua/plenary.nvim", plenary_dir)
-ensure_installed("https://github.com/nvim-treesitter/nvim-treesitter", treesitter_dir)
-
--- Add to runtime path
-vim.opt.rtp:prepend(plenary_dir)
-vim.opt.rtp:prepend(treesitter_dir)
-vim.opt.rtp:prepend(vim.fn.getcwd())
-
--- Load treesitter
+vim.cmd("runtime! plugin/plenary.vim")
 vim.cmd("runtime! plugin/nvim-treesitter.lua")
 
--- Install VHDL parser
 require("nvim-treesitter.configs").setup({
     ensure_installed = { "vhdl" },
     sync_install = true,
